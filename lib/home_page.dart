9966fff2021294e48213b3bd218fc41ad7f12a78
Ethan Sohail <sohailansari2009@gmail.com>
2022-04-27 18:23:13 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cataloge App"),
+      ),
+    body: Center(
+    child: Text("Hellow 3 World"),
+    ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myfistflutterproject/home_page.dart';
 
 void main(){
   runApp(MyApp());
@@ -8,11 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Text("Hellow World"),
-        ),
-      ),
+      home: HomePage()
     );
   }
 }
